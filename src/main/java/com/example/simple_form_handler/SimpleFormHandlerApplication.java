fb57431745183189b8c082d615f41c9b94d4ba45
ingvar22 <matveev556667@gmail.com>
2025-07-31 20:44:24 +0700

    The project has been created.

---
+++ src/main/java/com/example/simple_form_handler/SimpleFormHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_form_handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFormHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFormHandlerApplication.class, args);
+	}
+
+}
